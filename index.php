fe57846f93679b6e59ab60ecdbdfaa920168ef99
Ben <benjamin@bliss-interactive.com>
2020-08-21 10:45:43 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,466 @@
+<!DOCTYPE html>
+
+<!--
+** Proudly made by
+  ____  _      _                            _
+ |  _ \(_) ___| |____      _____   ___   __| |
+ | |_) | |/ __| '_ \ \ /\ / / _ \ / _ \ / _` |
+ |  _ <| | (__| | | \ V  V / (_) | (_) | (_| |
+ |_| \_\_|\___|_| |_|\_/\_/ \___/ \___/ \__,_|
+
+-->
+
+   <!-- A website entiery designed by Richwood -->
+   <!--[if IE]><script src="JS/html5-ie.js"></script><![endif]-->
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- FAVICONS -->
+		<link rel="icon" href="./img/flavicon.ico">
+		<link rel="apple-touch-icon" href="./img/flavicon.ico">
+
+		<!-- Font -->
+		<link href='https://fonts.googleapis.com/css?family=Sintony:400,700|Luckiest+Guy' rel='stylesheet' type='text/css'> <!-- Knewave -->
+
+		<!-- STYLE -->
+		<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+		<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+		<link rel="stylesheet" href="css/component.css"> <!-- morph button -->
+		<link rel="stylesheet" href="css/content.css"> <!-- morph button -->
+		<script src="js/modernizr.js"></script> <!-- Modernizr -->
+
+		<!-- ** OPEN GRAPH ** -->
+		<link rel="canonical" href="http://richebois.fr" />
+		<meta property="og:locale" content="en_UK" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Mark of Superheroes" />
+		<meta property="og:url" content="http://markofsuperheroes.com" />
+		<meta property="og:site_name" content="Richwood"/>
+		<meta property="og:description" content="A set of drawing about Superheroes's downsides" />
+		<meta property="og:image" content="http://markofsuperheroes.com/img/export/openGraph.jpg" />
+		<meta property="og:image:url" content="http://markofsuperheroes.com/img/export/openGraph.jpg" />
+		<meta property="og:image:width" content="1300" />
+		<meta property="og:image:height" content="730" />
+		<meta property="og:image" content="http://markofsuperheroes.com/img/export/openGraph_thumbnail.jpg" />
+		<meta property="og:image:url" content="http://markofsuperheroes.com/img/export/openGraph_thumbnail.jpg" />
+		<meta property="og:image:width" content="650" />
+		<meta property="og:image:height" content="350" />
+
+		<meta name="twitter:card" content="logo" />
+		<meta name="twitter:site" content="@richwood" />
+		<meta name="twitter:title" content="Mark of Superheroes" />
+		<meta name="twitter:description" content="A set of drawing about Superheroes's downsides" />
+		<meta name="twitter:image" content="http://markofsuperheroes.com/img/export/openGraph_thumbnail.jpg" />
+		<meta name="twitter:url" content="http://markofsuperheroes.com" />
+		<title>Mark of Superheroes</title>
+	</head>
+	<body>
+		<script defer src="js/main.js"></script> <!-- Resource jQuery -->
+		<script defer src="js/classie.js"></script> <!-- classes -->
+		<script defer src="js/uiMorphingButton_fixed.js"></script> <!-- UI morph button -->
+		<script defer type="text/javascript" src="./js/skrollr.min.js"></script>
+		<!-- <script defer type="text/javascript" src="./js/waypoint.min.js"></script> -->
+
+		<div class="buttonLang">
+			<div>lang</div>
+			<button onclick="changeLang('en-UK')">EN</button>
+			<button onclick="changeLang('fr-FR')">FR</button>
+		</div>
+		<nav class="cd-vertical-nav">
+			<ul>
+				<li><a href="#section1" class="active"><span class="label">Intro</span></a></li>
+				<li><a href="#section2"><span class="label">Captain</span></a></li>
+				<li><a href="#section3"><span class="label">Ironma</span></a></li>
+				<li><a href="#section4"><span class="label">Spidey</span></a></li>
+				<li><a href="#section5"><span class="label">Credit</span></a></li>
+			</ul>
+		</nav><!-- .cd-vertical-nav -->
+
+		<button class="cd-nav-trigger cd-image-replace">Open navigation<span aria-hidden="true"></span></button>
+
+		<section id="section1" class="cd-section intro">
+			<div class="content-wrapper">
+				<h1>Mark of Superheroes</h1>
+				<img src="./img/logo_SH.png" alt="logo Mark of Superheores" style="width: 120px;">
+				<div class="textExplanation">
+					<h3>Manifest</h3>
+					<!-- <p>L'idee de ce projet est de questioner un autre aspects des superheros. Derriere les costumes flashy et moulants, les exploits heroiques, les poses charismatiques et les punchlines inspirantes, on ne parle pas beaucoup des degats que ces super-heros laissent derriere eux. Cette serie de dessin propose quelques idees de conflits que peuvent laisser les heros, conflits entre les agents publiques et les particuliers. Qui est vraiment responsable de leur actes. Sont-ils des agents d'utilites publiques ? Apres tout ce sont les contribuables qui payent pour reparer leurs degats.</p> -->
+
+					<p class="manifestStyle">
+						The main idea of this project is to question another aspect of superheroes. Beyond flashy and skin-tight costumes, heroic feats and inspiring punchlines, downsides and damages are not frequently mentioned. This serie of drawings suggests few conflicts between superheroes and public utility or private individuals. Who is in charge of their actions? Are they always working for the common good? After all, taxpayers pay for their damages.
+					</p>
+				</div>
+				<a href="#section2" class="cd-scroll-down cd-image-replace">scroll down</a>
+			</div>
+		</section><!-- cd-section -->
+
+		<section id="section2" class="cd-section">
+			<div class="content-wrapper">
+				<div class="SHtitle" data--200-bottom="display:block;">
+					<h2>Captain</h2>
+					<hr class="hrAnimation">
+				</div>
+
+				<div class="divImg">
+					<div class="parallaxIMG">
+						<div data-170-bottom="transform:translateY(90px);" data--400-top="transform:translateY(-80px);" style="background-image:url(./img/Captain/abri-bus.png);"></div>
+						<div data-180-bottom="transform:translateY(160px);" data--500-top="transform:translateY(-180px);"style="background-image:url(./img/Captain/voiture.png);"></div>
+						<div data-200-bottom="transform:translateY(180px);" data--500-top="transform:translateY(-210px);" style="background-image:url(./img/Captain/assure.png);"></div>
+						<div data-200-bottom="transform:translateY(200px);" data--500-top="transform:translateY(-250px);"style="background-image:url(./img/Captain/assureur.png);"></div>
+					</div>
+
+					<div class="morph-button morph-button-modal morph-button-modal-3 morph-button-fixed">
+						<button type="button">Download</button>
+						<div class="morph-content">
+							<div>
+								<div class="content-style-form content-style-form-2">
+									<span class="icon icon-close">Close the popup</span>
+									<h2>Download the images</h2>
+									<!-- <p>Links to download the image</p> -->
+									<table>
+										<tr>
+											<td>
+												<ul>
+													<li><a target="_blank" href="./img/export/Captain_4961x3508.jpg" title="4961 x 3508">4961 x 3508</a></li>
+													<li><a target="_blank" href="./img/export/Captain_3000x2121.jpg" title="3000 x 2121">3000 x 2121</a></li>
+												</ul>
+											</td>
+											<td>
+												<img src="./img/export/Captain_thumbnail.jpg">
+											</td>
+										</tr>
+									</table>
+
+							</div>
+							<div class="creativeCommons">
+								<p class="subtitle">
+									<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+								</p>
+							</div>
+							</div>
+						</div><!-- morph-button -->
+					</div>
+
+				</div>
+			</div>
+		</section><!-- cd-section -->
+
+
+		<section id="section3" class="cd-section" style="background-color:#FFF;" data-bottom-top="background-color:rgb(255,255,255);" data-center="background-color:rgb(0,0,0);" data-bottom="background-color:rgb(255,255,255);">
+			<div class="content-wrapper">
+				<h2 class="SHtitle">Iron man</h2>
+				<hr class="hrAnimation">
+
+				<div class="divImg">
+					<img src="./img/ironMan/Iron-man04.png">
+
+					<div class="morph-button morph-button-modal morph-button-modal-3 morph-button-fixed">
+						<button type="button">Download</button>
+						<div class="morph-content">
+							<div>
+								<div class="content-style-form content-style-form-2">
+									<span class="icon icon-close">Close the popup</span>
+									<h2>Download the images</h2>
+									<!-- <p>Links to download the image</p> -->
+									<table>
+										<tr>
+											<td>
+												<ul>
+													<li><a target="_blank" href="./img/export/Ironman_4961x3508.jpg" title="4961 x 3508">4961 x 3508</a></li>
+													<li><a target="_blank" href="./img/export/Ironman_3000x2620.jpg" title="3000 x 2121">3000 x 2620</a></li>
+												</ul>
+												<h4>With smoke</h4>
+												<ul>
+													<li><a target="_blank" href="./img/export/Ironman-smoke_3076x3389.jpg" title="3076 x 3389">3076 x 3389</a></li>
+												</ul>
+											</td>
+											<td>
+												<img src="./img/export/Iron-man_thumbnail.jpg">
+											</td>
+										</tr>
+									</table>
+
+							</div>
+							<div class="creativeCommons">
+								<p class="subtitle">
+									<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+							</p>
+						</div>
+						</div>
+					</div><!-- morph-button -->
+				</div>
+
+				</div>
+
+			</div>
+		</section><!-- cd-section -->
+
+
+
+
+
+		<section id="section4" class="cd-section">
+			<div class="content-wrapper">
+				<h2 class="SHtitle">Spidey</h2>
+				<hr class="hrAnimation">
+
+				<div class="divImg">
+					<img src="./img/spidey/spidey02.png">
+
+
+					<div class="morph-button morph-button-modal morph-button-modal-3 morph-button-fixed">
+						<button type="button">Download</button>
+						<div class="morph-content">
+							<div>
+								<div class="content-style-form content-style-form-2">
+									<span class="icon icon-close">Close the popup</span>
+									<h2>Download the images</h2>
+									<!-- <p>Links to download the image</p> -->
+									<table>
+										<tr>
+											<td>
+												<ul>
+													<li><a target="_blank" href="./img/export/spidey_4823x3031.jpg" title="4823 x 3508">4961 x 3031</a></li>
+													<li><a target="_blank" href="./img/export/spidey_3000x1885.jpg" title="3000 x 2121">3000 x 1885</a></li>
+												</ul>
+											</td>
+											<td>
+												<img src="./img/export/spidey_thumbnail.jpg">
+											</td>
+										</tr>
+									</table>
+
+							</div>
+							<div class="creativeCommons">
+								<p class="subtitle">
+									<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+							</p>
+						</div>
+						</div>
+					</div><!-- morph-button -->
+				</div>
+
+				</div>
+			</div>
+		</section><!-- cd-section -->
+
+		<section id="section4" class="cd-section">
+			<div class="content-wrapper">
+				<h2 class="SHtitle">Man of steel</h2>
+				<hr class="hrAnimation">
+
+					<div class="parallaxFirewatch">
+						<div data-170-bottom="transform:translateY(90px);" data--400-top="transform:translateY(-80px);" style="background-image:url(./img/Layers/layer6.png);"></div>
+						<div data-180-bottom="transform:translateY(160px);" data--500-top="transform:translateY(-180px);"style="background-image:url(./img/Layers/layer5.png);"></div>
+						<div data-200-bottom="transform:translateY(180px);" data--500-top="transform:translateY(-210px);" style="background-image:url(./img/Layers/layer4.png);"></div>
+						<div data-200-bottom="transform:translateY(200px);" data--500-top="transform:translateY(-250px);"style="background-image:url(./img/Layers/layer3.png);"></div>
+						<div data-200-bottom="transform:translateY(200px);" data--500-top="transform:translateY(-250px);"style="background-image:url(./img/Layers/layer2.png);"></div>
+						<div data-200-bottom="transform:translateY(200px);" data--500-top="transform:translateY(-250px);"style="background-image:url(./img/Layers/layer1b.png);"></div>
+					</div>
+
+				<!-- <div class="">
+
+					<div class="parallax__layer parallax__layer__0">
+					 <img src="./img/Layers/layer2.png" />
+				 </div>
+				 <div class="parallax__layer parallax__layer__1">
+						 <img src="./img/Layers/layer3.png" />
+				 </div>
+				 <div class="parallax__layer parallax__layer__2">
+						 <img src="./img/Layers/layer4.png" />
+				 </div>
+				 <div class="parallax__layer parallax__layer__3">
+						 <img src="./img/Layers/layer5.png" />
+				 </div>
+				 <div class="parallax__layer parallax__layer__4">
+						 <img src="./img/Layers/layer6.png" />
+				 <!-- </div>
+				 <div class="parallax__layer parallax__layer__5">
+						 <img src="https://sam.beckham.io/images/articles/firewatch/layer_5.png" />
+				 </div>
+				 <div class="parallax__layer parallax__layer__6">
+						 <img src="https://sam.beckham.io/images/articles/firewatch/layer_6.png" /> -->
+				 <!-- </div>
+				 <div class="parallax__cover"></div>
+				</div> -->
+
+			</div>
+		</section><!-- cd-section -->
+
+		<section id="section5" class="cd-section">
+			<div class="content-wrapper">
+				<h2 class="SHtitle">Credit</h2>
+				<hr class="hrAnimation">
+
+				<div class="divImg">
+					<div class="textCredit manifestStyle">
+						<p>
+						</p>
+
+
+						<ul>
+							<li><a href="https://github.com/Prinzhorn/skrollr" target="_blank">Skrollr</a></li>
+							<li><a href="https://github.com/imakewebthings/waypoints" target="_blank">Waypoint</a></li>
+							<li>Photoshop</li>
+							<li><a href="https://tympanus.net/codrops/2014/05/12/morphing-buttons-concept/" target="_blank">Codrops: Morphing Buttons Concept</a></li>
+							<li><a href="https://codyhouse.co/gem/vertical-fixed-navigation-2/" target="_blank">Codyhouse: Vertical Fixed Animation</a></li>
+						</ul>
+					</div>
+
+
+				</div>
+			</div>
+		</section><!-- cd-section -->
+
+
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="pull-right">
+						Made by <a href="https://richebois.fr/?lang=en">Richwood</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+
+		<script src="js/jquery-2.1.4.js"></script>
+
+		<script>
+			var section2 = document.getElementById('section2'),
+			section3 = document.getElementById('section3'),
+			section4 = document.getElementById('section4'),
+			section5 = document.getElementById('section5');
+
+			var text = {
+				fr: {
+					manifestTitle: "Manifeste",
+					manifest: "L'idée principale derrière ce projet est de questionner un autre aspect des superhéros. Au delà des costumes moulants et flashy, des poses héroïques et des punchlines, les dommages et les dégâts sont rarement mentionnés. Cette série de dessin met en scène des conflits entre les interventions des superhéroes et les services publiques ou les individus. Qui est responsables de leurs actions ? Est-ce vraiment pour systématiquement pour le bien commun ? Après tout, ce sont les contribuables qui payent.",
+					credit: "Réalisé en avril 2018 par Richwood.<br>Réalisé avec :",
+					dlTitle: "Télécharger les fichiers sources",
+					dl: "Liens pour télécharger l'image :",
+				},
+				en: {
+					manifestTitle: "Manifest",
+					manifest: "The main idea of this project is to question another aspect of superheroes. Beyond flashy and skin-tight costumes, heroic feats and inspiring punchlines, downsides and damages are not frequently mentioned. This serie of drawings suggests few conflicts between superheroes and public utility or private individuals. Who is in charge of their actions? Are they always working for the common good? After all, taxpayers pay for their damages.",
+					credit: "Made by Richwood in April 2018.<br>Made with:",
+					dlTitle: "Download assets",
+					dl: "Link to download the image:",
+				},
+
+			}
+
+			window.onload = function() {
+
+				if(window.innerWidth > 800 ) var s = skrollr.init();
+
+				// var waypoint = new Waypoint({
+				//   element: section2,
+				//   handler: function(direction) {
+				// 		section2.getElementsByClassName('SHtitle')[0].style.display='block';
+				//   }
+				// })
+
+
+				var manifestBlock = document.querySelector('.textExplanation p'),
+				manifestTitle = document.querySelector('.textExplanation h3'),
+				creditBlock = document.querySelector('.textCredit p:first-child'),
+				dlTitle = document.querySelectorAll('.content-style-form > h2');
+
+
+				// (function() {
+				var docElem = window.document.documentElement, didScroll, scrollPosition;
+
+				// trick to prevent scrolling when opening/closing button
+				function noScrollFn() {
+					window.scrollTo( scrollPosition ? scrollPosition.x : 0, scrollPosition ? scrollPosition.y : 0 );
+				}
+
+				function noScroll() {
+					window.removeEventListener( 'scroll', scrollHandler );
+					window.addEventListener( 'scroll', noScrollFn );
+				}
+
+				function scrollFn() {
+					window.addEventListener( 'scroll', scrollHandler );
+				}
+
+				function canScroll() {
+					window.removeEventListener( 'scroll', noScrollFn );
+					scrollFn();
+				}
+
+				function scrollHandler() {
+					if( !didScroll ) {
+						didScroll = true;
+						setTimeout( function() { scrollPage(); }, 60 );
+					}
+				};
+
+				function scrollPage() {
+					scrollPosition = { x : window.pageXOffset || docElem.scrollLeft, y : window.pageYOffset || docElem.scrollTop };
+					didScroll = false;
+				};
+
+				scrollFn();
+
+				[].slice.call( document.querySelectorAll( '.morph-button' ) ).forEach( function( bttn ) {
+					new UIMorphingButton( bttn, {
+						closeEl : '.icon-close',
+						onBeforeOpen : function() {
+							// don't allow to scroll
+							noScroll();
+						},
+						onAfterOpen : function() {
+							// can scroll again
+							canScroll();
+						},
+						onBeforeClose : function() {
+							// don't allow to scroll
+							noScroll();
+						},
+						onAfterClose : function() {
+							// can scroll again
+							canScroll();
+						}
+					} );
+				} );
+
+				changeLang( window.navigator.language.toString() );
+
+				function changeLang(lang){
+					var language = lang.match(/^fr/) ? 'fr' : 'en';
+					manifestTitle.innerHTML = text[language].manifestTitle;
+					manifestBlock.innerHTML = text[language].manifest;
+					creditBlock.innerHTML = text[language].credit;
+					for (var i = dlTitle.length - 1; i >= 0; i--) {
+						dlTitle[i].innerHTML = text[language].dlTitle;
+					}
+				}
+
+					// document.getElementById( 'terms' ).addEventListener( 'change', function() {
+					// 	UIBtnn.toggle();
+					// } );
+
+
+					// document.querySelector('.divImg').addEventListener('contextmenu', function(ev) {
+					//     ev.preventDefault();
+					//     // alert('success!');
+					//     UIBtnn.toggle();
+					//     return false;
+					// }, false);
+
+				// })();
+				// var ev = document.querySelectorAll('div.divImg');
+				// function closeDownloadPopup(e){
+				// 	// ev[e].preventDefaultult();
+				// 	// alert("Success");
+				// 	UIBtnn.toggle();
+				//     return false;
+				// }
+			}
+		</script>
+	</body>
+</html>
